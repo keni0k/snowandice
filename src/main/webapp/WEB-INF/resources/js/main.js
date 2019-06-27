2ba334f65ec0000cea7ac5646ad368b0353d8833
keni0k <dima-vers0@rambler.ru>
2019-06-27 14:27:27 +0700

    anchors scroll + less padding between cards

--- src/main/webapp/WEB-INF/resources/js/main.js
+++ src/main/webapp/WEB-INF/resources/js/main.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function($){
     });    
     
     // jQuery Scroll effect
-    $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
+    /*$('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
         var $anchor = $(this);
         var headerH = $('.header-area').outerHeight();
         $('html, body').stop().animate({
@@ -18,7 +18,26 @@ jQuery(document).ready(function($){
         }, 1200, 'easeInOutExpo');
 
         event.preventDefault();
-    });    
+    });*/
+
+    var hashTagActive = "";
+    $(".js-scroll-trigger").on("click touchstart" , function (event) {
+        if(hashTagActive !== this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 2000, 'swing');
+            hashTagActive = this.hash;
+        }
+    });
     
     // Bootstrap ScrollPSY
     $('body').scrollspy({ 
